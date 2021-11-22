a9da2f952d1d56073ade32c89e6a46ae838ee3dd
agral <adam@gralin.ski>
2021-11-22 23:51:27 +0100

    Add sample personal data class to python interp

---
+++ pythonx/sample_personal_data.py
@@ -0,0 +1,20 @@
+# Name:          sample_personal_data.py
+# Options:       None
+# Created on:    22.11.2021
+# Last modified: 22.11.2021
+# Author:        Adam Graliński (https://gralin.ski)
+# License:       MIT
+
+"""
+Encapsulates personal data that should not be committed to opensource.
+This file should be edited so that it holds your data,
+and then saved as `personal_data.py` here.
+"""
+
+class PersonalData:
+    # Holds email addresses; `mail` snippet will get expanded to one of these:
+    EMAIL_ADDRESSES = [
+            "john@doe.com",
+            "john@example.org",
+            "j.doe@business.com",
+    ]
